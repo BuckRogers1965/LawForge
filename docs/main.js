169881e1d64cde196748dbcdf8bb6c92d4e26783
James Rogers <jamesmrogers@gmail.com>
2025-07-12 18:49:56 -0400

    trying to get existing law drop down to work.

--- docs/main.js
+++ docs/main.js
@@ -11,8 +11,9 @@ PLANCK_UNITS = {
     'm_P': sqrt(h * c / G), 'l_P': sqrt(h * G / c**3), 't_P': sqrt(h * G / c**5),
     'T_P': sqrt(h * c**5 / (G * k_B**2)), 'E_P': sqrt(h * c**5 / G), 'F_P': c**4 / G,
     'P_P': c**5 / G, 'rho_P': c**7 / (h * G**2), 'p_P': sqrt(h * c**3 / G),
-    'a_P': sqrt(c**7 / (h * G)), 'f_P': sqrt(c**5 / (h * G)), 'v_P': c,
+    'f_P': sqrt(c**5 / (h * G)), 'v_P': c, 'a_P': l_P / t_P**2, 
 }
+#'a_P': sqrt(c**7 / (h * G))
 VARIABLE_TO_PLANCK_UNIT = {
     'M': 'm_P', 'M1': 'm_P', 'M2': 'm_P', 'm': 'm_P',
     'r': 'l_P', 'l': 'l_P', 'x': 'l_P', 'lambda': 'l_P', 'r_s': 'l_P',
